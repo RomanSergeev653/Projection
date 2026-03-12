848684e5b4448c574f2e66d82f39a2f98ab9c59c
RomanSergeev653 <romansergeev653@yandex.ru>
2026-03-12 19:02:15 +0300

    Подготовка к деплою 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/projection/',
+  server: {
+    host: '0.0.0.0', // Слушать на всех интерфейсах
+    port: 5173,
+    strictPort: true, // Не использовать другой порт если 5173 занят
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
